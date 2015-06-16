41fb4bd1a89c83c05908a5b630e46528fed8379d
quangvinh93hcm <vinh.dq@felix.vn>
2015-06-16 09:28:16 +0700

    update ICategory

---
+++ QLBanHang/Interface/ICategory.cs
@@ -0,0 +1,17 @@
+﻿using QLBanHang.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QLBanHang.Interface
+{
+    public interface ICategory
+    {
+        void Insert(Category category);
+        void Delete(int id);
+        void Update(Category category);
+        void Save();
+    }
+}
