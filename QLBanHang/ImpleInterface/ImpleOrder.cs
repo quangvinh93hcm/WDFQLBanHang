b31a9ed24550ddc2c4be4f96f14e11175cb89e23
quangvinh93hcm <vinh.dq@felix.vn>
2015-06-24 13:55:26 +0700

    updated

---
+++ QLBanHang/ImpleInterface/ImpleOrder.cs
@@ -0,0 +1,46 @@
+﻿using QLBanHang.Interface;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using QLBanHang.Model;
+
+namespace QLBanHang.ImpleInterface
+{
+    public class ImpleOrder : IOrder
+    {
+        private Entities _context;
+
+        public ImpleOrder(Entities context)
+        {
+            _context = new Entities();
+        }
+
+        public void Insert(Order order)
+        {
+            try
+            {
+                _context.Orders.Add(order);
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+
+        public void Save()
+        {
+            try
+            {
+                _context.SaveChanges();
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+
+        }
+    }
+}
---
+++ QLBanHang/Interface/IOrder.cs
@@ -0,0 +1,18 @@
+﻿using QLBanHang.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QLBanHang.Interface
+{
+    public interface IOrder
+    {
+        void Insert(Order order);
+        /*IEnumerable<Order> GetOrderByID();
+        IEnumerable<Order> GetOrderByDate();
+        IEnumerable<Order> GetOrderByEmployeeID(string id);*/
+        void Save();
+    }
+}
